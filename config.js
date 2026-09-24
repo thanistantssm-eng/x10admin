39aa9e36ddc42b030a7c81e30c8c558ef8e85224
thanistantssm-eng <thanistantssm@gmail.com>
2026-09-24 12:02:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.X10_ADMIN_CONFIG = Object.assign({ API_BASE: '/api' }, window.X10_ADMIN_CONFIG || {});
+window.X10_ADMIN_CONFIG = Object.assign({ API_BASE: 'https://api.x10think.online/api' }, window.X10_ADMIN_CONFIG || {});
